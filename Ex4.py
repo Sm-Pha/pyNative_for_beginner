91011033ed78173f977644fe74f17c02ea8d7be4
Sm-Pha <sumittra.pt@gmail.com>
2023-08-30 14:45:32 +0700

    5 set exercises

---
+++ Ex4.py
@@ -0,0 +1,5 @@
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+
+similar_ = set1.difference_update(set2)
+print(f"set1 {set1}")
---
+++ Ex5.py
@@ -0,0 +1,6 @@
+set1 = {10, 20, 30, 40, 50}
+remove_item = {10, 20, 30}
+
+set1.difference_update(remove_item)
+print(set1)
+
