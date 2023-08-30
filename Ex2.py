f298002171e26091319b3e5959d9f8d9c76121a6
Sm-Pha <sumittra.pt@gmail.com>
2023-08-30 15:04:10 +0700

    update branch

--- Ex2.py
+++
@@ -1,9 +0,0 @@
-list1 = ["M", "na", "i", "Ke"]
-list2 = ["y", "me", "s", "lly"]
-
-combine_ = zip(list1, list2)
-
-result_ = [ f"{i}{j}" for i,j in combine_]
-print(result_)
-
-
--- Ex5.py
+++
@@ -1,8 +0,0 @@
-list1 = [10, 20, 30, 40]
-list2 = [100, 200, 300, 400]
-
-list2.reverse()
-new_list2 = list2
-
-new_list = zip(list1, new_list2)
-print(list(new_list))
