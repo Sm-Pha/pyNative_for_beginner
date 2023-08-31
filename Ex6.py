fdc73568996a7b5b5e44099982b7e571fcf593a8
Sm-Pha <sumittra.pt@gmail.com>
2023-08-31 22:04:36 +0700

    next 4 set exercises

---
+++ Ex6.py
@@ -0,0 +1,5 @@
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+unique_ = set1.symmetric_difference(set2)
+print(unique_)
---
+++ Ex7.py
@@ -0,0 +1,5 @@
+set1 = {10, 20, 30, 40, 50}
+set2 = {60, 70, 80, 90, 10}
+
+print(f"Two sets have items in common "
+      f"\n{set1.intersection(set2)}")
